5c1293f75dd388cf0aae625147a65f5f884bdea7
chenyingcai <cheningcai@outlook.com>
2018-04-13 02:05:15 +0200

    modify the build_hugo_minify

--- build_hugo_minify.sh
+++ build_hugo_minify.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 DOCKER_NAME='chenyingcai/hugo_busybox_minify:v1'
-HUGO_VERSION=0.20.2
 if [ "$(docker images -q $DOCKER_NAME 2> /dev/null)" == "" ]; then
   echo "没有找到$DOCKER_NAME容器"
   echo "----------------------------------------------"
   echo "下载相应的临时文件"
-  wget https://github.com/chenyingcai/hugo_blog/master/buildfiles/minify
-  cd minify
-  curl -o Dockerfile_hugo_minify https://raw.githubusercontent.com/chenyingcai/hugo_blog/master/buildfiles/Dockerfile_hugo_minify
+  wget https://github.com/chenyingcai/hugo_blog/archive/master.zip
+  unzip master.zip
+  MAIN_DIR=$PWD
+  cd hugo_blog-master/buildfiles/minify
   echo "开始创建$DOCKER_NAME容器"
-  docker build -t $DOCKER_NAME -f Dockerfile_hugo
-  cd ..
+  docker build -t $DOCKER_NAME -f Dockerfile_hugo_minify .
+  cd $MAIN_DIR
   echo "删除临时文档"
-  rm -rf minify
+  rm -rf hugo_blog-master master.zip
   echo "完成"
 else
   echo "已经安装$DOCKER_NAME"
