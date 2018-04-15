7b580b0b0c7e57010a385d06b91c6e7e81c9b8c5
chenyingcai <cheningcai@outlook.com>
2018-04-15 16:45:53 +0200

    add hugo with demo site docker image

--- build_hugo_demo.sh
+++ build_hugo_demo.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 DOCKER_NAME='chenyingcai/hugo_busybox:v1'
 REPONAME='demo'
+BASE_URL='localhost:8000'
 SPERA="===================================================="
 HUGO_VERSION=0.20.2
 if [ "$(docker images -q $DOCKER_NAME 2> /dev/null)" == "" ]; then
@@ -11,7 +12,7 @@ if [ "$(docker images -q $DOCKER_NAME 2> /dev/null)" == "" ]; then
   wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
   tar xf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
   mv hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 hugo
-  echo -e "#! /bin/sh\nhugo server --baseURL=$BASE_URL --bind=0.0.0.0 --appendPort=false" > run.sh
+  echo -e "#! /bin/sh\nhugo server --baseURL=\$BASE_URL --bind=0.0.0.0 --appendPort=false" > run.sh
   echo "开始创建$DOCKER_NAME容器"
   docker build -t $DOCKER_NAME -f Dockerfile_hugo .
   echo "删除临时文档"
@@ -27,23 +28,27 @@ echo "注意: 使用SVN可以下载指定文件夹而不用克隆整个项目"
 svn checkout https://github.com/chenyingcai/hugo_blog/trunk/tranquilpeak $PWD/resource/tranquilpeak
 echo "下载demo文档"
 svn checkout https://github.com/chenyingcai/hugo_blog/trunk/exampleSite $PWD/resource/exampleSite
+echo "创建copy样本shell文件"
+echo -e "#! /bin/sh\ncp -rf /example/* /hugo/\nhugo server --bind=0.0.0.0 --appendPort=false --baseURL=\$BASE_URL" > democopy.sh
 echo "创建DockerFile:Dockerfile_${REPONAME}"
 echo """FROM chenyingcai/hugo_busybox:v1
-ENV BASE_URL=localhost:8000
+ENV BASE_URL=$BASE_URL
+RUN hugo new site /example && mkdir -p /example/build
+COPY . /example/build/
+RUN cp -rf /example/build/resource/tranquilpeak /example/themes/ && \\
+    cp -rf /example/build/resource/exampleSite/* /example/ && \\
+    chmod a+x /example/build/democopy.sh && \\
+    cp -rf /example/build/democopy.sh /bin/ && \\
+    rm -rf /example/build 
 
 WORKDIR /hugo
-RUN hugo new site ./
-COPY . /hugo/
-RUN mv -f /hugo/resource/tranquilpeak /hugo/themes/ && \\
-    cp -rf /hugo/resource/exampleSite/* /hugo/ && \\
-    rm -rf /hugo/resource /hugo/Dockerfile_${REPONAME}
 VOLUME [/hugo/]
 EXPOSE 1313
 CMD [run.sh]""" > Dockerfile_${REPONAME}
 echo "创建chenyingcai/hugo_${REPONAME}:v1镜像"
 docker build -t chenyingcai/hugo_${REPONAME}:v1 -f Dockerfile_${REPONAME} .
 echo "删除临时文档"
-rm -rf tranquilpeak exampleSite Dockerfile_${REPONAME} resource
+rm -rf Dockerfile_${REPONAME} resource democopy.sh
 echo "done"
 echo $SPERA
-echo -e "使用方法:\n 1. docker run -itd --rm --name hugoimg -p 8000:1313 -v \$PWD:/hugo/ chenyingcai/hugo_${REPONAME}:v1\n 2. 使用alias hugo= \"docker exec hugoimg hugo\"赋值hugo命令, 之后使用hugo直接使用其他操作命令"
+echo -e "使用方法:\n 1. docker run -itd --rm --name hugoimg -p 8000:1313 -v \$PWD/$REPONAME:/hugo/ chenyingcai/hugo_${REPONAME}:v1\n 2. 使用alias hugo= \"docker exec hugoimg hugo\"赋值hugo命令, 之后使用hugo直接使用其他操作命令\n 3. 使用docker exec hugoimg democopy.sh获取样板网页以修改创建自己的网页"
